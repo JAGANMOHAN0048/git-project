504ec2d8bb1e2f51348ee668e39869fa4751d569
JAGANMOHAN0048 <jaganmohan0048@gmail.com>
2023-09-11 13:09:58 +0530

    gggg

--- Calci.java
+++ Calci.java
@@ -1,5 +1,16 @@
 Interface Calci
 {
-	public void add(int a, int b);
-	public void sub(int a, int b);
+	public int add(int a, int b);
+	public int sub(int a, int b);
+}
+class CalciImpl implements Calci
+{
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
 }
