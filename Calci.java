6740a0d024c6cc37d6ca35fd407566a4d529befa
JAGANMOHAN0048 <jaganmohan0048@gmail.com>
2023-09-11 13:40:26 +0530

    CalciDriver Class is Created

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 Interface Calci
 {
 	public int add(int a, int b);
@@ -12,5 +13,19 @@ class CalciImpl implements Calci
 	public int sub(int a, int b)
 	{
 		return a-b;
+	}
+}
+class CalciDriver
+{
+	public static void main(String arg[])
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int n1 = s.nextInt();
+		int n2 = s.nextInt();
+		CalciImpl i = new CalciImpl();
+		System.out.println(n1 + " + " + n2 + " = " + i.add(n1,n2));
+		System.out.println(n1 + " - " + n2 + " = " + i.sub(n1,n2));
+
 	}
 }
